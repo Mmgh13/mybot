2e680130cd7956bd7deb59049de449c5dc8ae5cd
py_mahdi <mmghaioumi8513@gmail.com>
2025-07-16 16:28:20 +0330

    add filters private

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,15 +1,16 @@
 from pyrogram import Client, filters, enums
-
+import time
 
 
 texts = {
-    'start': "سلام من ربات **عارینح** هستم😎😁\n امیدوارم که بتونم به بهترین نحوه به **امیرحسن عارینح** --انرژی-- بدم \n فقط کافیه من رو توی گروهی که عارینح هست عضو کنی❤️🫡"
+    'start': "سلام من ربات **عارینح** هستم😎😁\n امیدوارم که بتونم به بهترین نحوه به **امیرحسن عارینح** --انرژی-- بدم. \n فقط کافیه من رو توی گروهی که عارینح هست عضو کنی❤️🫡!!"
 }
 
 
 
 # start commands
-@Client.on_message(filters.command(["start", 'help']))
+@Client.on_message(filters.command(["start", 'help']) & filters.private)
 async def start(client, message):
     await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-    await message.reply(texts['start'], parse_mode=enums.ParseMode.MARKDOWN)
+    time.sleep(2)
+    await message.reply_text(texts['start'], parse_mode=enums.ParseMode.MARKDOWN)
