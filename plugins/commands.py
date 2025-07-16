9136d752c7111d0fba34d8ed9cb114048f35cdea
py_mahdi <mmghaioumi8513@gmail.com>
2025-07-16 15:56:18 +0330

    create hello command

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,14 @@
 from pyrogram import Client, filters
 
+
+
+texts = {
+    'start': "سلام من ربات **عارینح** هستم😎😁\n امیدوارم که بتونم به بهترین نحوه به **امیرحسن عارینح** --انرژی-- بدم"
+}
+
+
+
 # start commands
-@Client.on_message(filters.command("start") & filters.text)
+@Client.on_message(filters.command(["start", 'help']))
 async def start(client, message):
-    await message.reply('hello')
+    await message.reply_text(texts['start'])
